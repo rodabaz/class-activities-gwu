7e4c2158739d355114ae9a52cc2926178f912d56
rodabaz <rodabaz@gmail.com>
2023-01-12 18:11:55 -0500

    19.1

--- 19.1-activities/03_Using_jQuery/unsolved/js/index.js
+++ 19.1-activities/03_Using_jQuery/unsolved/js/index.js
@@ -1,5 +1,13 @@
 // Use jQuery to target the <h1> tag and make it red.
 
+$("h1");
+
+
 // Use jQuery to target the blue class and make it blue.
 
+$(".className")
+
 // Use jQuery to target the addMe ID and change its HTML to Hello.
+$("#idName");
+
+
