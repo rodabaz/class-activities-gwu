c2d5ceb50e084dd83a64078cd22b1ce110fe6cbb
rodabaz <rodabaz@gmail.com>
2023-01-12 22:05:49 -0500

    19.2

--- 19.2-activities/01_Conditionals/unsolved/js/index.js
+++ 19.2-activities/01_Conditionals/unsolved/js/index.js
@@ -1,3 +1,25 @@
 $('.tab').on("click", function(){
   
+    if ($(this).hasClass("tab1")) {
+        $(".tab1").addClass("active");
+        $(".tab1").removeClass("active");
+        $("#tabContent1").css("display","block");
+        $("#tabContent2").css("display","none");
+        } 
+    
+
+
+    else if ($(this).hasClass("tab2")) { 
+         $(".tab1").removeClass("active");
+         $(".tab2").addClass("active"); 
+         $("#tabContent1").css("display","none");
+         $("#tabContent2").css("display","block");
+    }
+    
+   
+
+
+
 });
+
+
